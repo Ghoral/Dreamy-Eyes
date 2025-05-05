92ba2acb450f081a633dbee73716f0686fdeb35e
Lovish Hamal <lovish@Lovishs-MacBook-Pro.local>
2025-05-05 13:24:50 +0545

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["https://whimsical-medovik-06e67e.netlify.app/", "localhost"],
+    domains: [
+      "https://whimsical-medovik-06e67e.netlify.app/",
+      "https://tgtjlmewzrumalciimkh.supabase.co",
+      "https://tgtjlmewzrumalciimkh.supabase.co/storage/v1/object/public/lens-images",
+    ],
   },
 };
 
