c0967c617bd5af61f2c9c5bb3da97cd56d32cf73
Lovish Hamal <lovish@Lovishs-MacBook-Pro.local>
2025-10-05 11:52:51 +0545

    fix: errors

--- app/services/supabase/client/supabaseBrowserClient.ts
+++ app/services/supabase/client/supabaseBrowserClient.ts
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { Database } from "@/app/types/database.types";
+
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createSupabaseClient() {
   // Create a supabase client on the browser with project's credentials
-  return createBrowserClient<Database>(
+  return createBrowserClient<any>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
