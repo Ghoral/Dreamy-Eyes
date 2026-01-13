69224f84f55e6dd4495503a2bce8d30f2c288695
Ghoral <lovishhamal456@gmail.com>
2026-01-13 20:00:45 +0545

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
       <BillboardCarousel />
       <ItemListing />
       <SalesSection />
-      <TikTokCarousel />
       <ProductWrapper />
       <AccessoriesSection />
+      <TikTokCarousel />
       <Footer />
     </div>
   );
